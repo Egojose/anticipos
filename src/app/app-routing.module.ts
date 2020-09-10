3a35b44df12cfb1cc42fc68f0d60cdeea77cc83e
Egojose <josemicrobiologo@gmail.com>
2020-09-10 09:05:12 -0500

    agrega rutas home y solicitar anticipos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SolicitarAnticiposComponent } from './solicitar-anticipos/solicitar-anticipos.component';
+import { AppComponent } from './app.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: AppComponent},
+  {path: 'solicitar-anticipo', component: SolicitarAnticiposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
