23b6ab3e967be24afa8c1a819c9ff7893b93a8f5
Egojose <josemicrobiologo@gmail.com>
2020-09-22 13:35:08 -0500

    agrega rutas a componentes nuevos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SolicitarAnticiposComponent } from './solicitar-anticipos/solicitar-anticipos.component';
 import { AppComponent } from './app.component'
+import { MisPendientesComponent } from './mis-pendientes/mis-pendientes.component';
+import { AprobarAnticipoComponent } from './aprobar-anticipo/aprobar-anticipo.component';
+import { ConsultarAnticiposComponent } from './consultar-anticipos/consultar-anticipos.component';
 
 
 const routes: Routes = [
   {path: 'home', component: AppComponent},
-  {path: 'solicitar-anticipo', component: SolicitarAnticiposComponent}
+  {path: 'solicitar-anticipo', component: SolicitarAnticiposComponent},
+  {path: 'mis-pendientes', component: MisPendientesComponent},
+  {path: 'aprobar-anticipo', component: AprobarAnticipoComponent},
+  {path: 'consultar-anticipos', component: ConsultarAnticiposComponent}
 ];
 
 @NgModule({
