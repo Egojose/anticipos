bc4879f9415b9ca40af8e13458beacef87dbd21d
Egojose <josemicrobiologo@gmail.com>
2020-10-02 14:55:09 -0500

    crea rutas para nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,21 @@ import { AppComponent } from './app.component'
 import { MisPendientesComponent } from './mis-pendientes/mis-pendientes.component';
 import { AprobarAnticipoComponent } from './aprobar-anticipo/aprobar-anticipo.component';
 import { ConsultarAnticiposComponent } from './consultar-anticipos/consultar-anticipos.component';
-
+import { LegalizarAnticipoComponent } from './legalizar-anticipo/legalizar-anticipo.component';
+import { DesembolsarAnticipoComponent } from './desembolsar-anticipo/desembolsar-anticipo.component';
+import { EditarLegalizacionComponent } from './editar-legalizacion/editar-legalizacion.component';
+import { AprobarLegalizacionComponent } from './aprobar-legalizacion/aprobar-legalizacion.component';
 
 const routes: Routes = [
   {path: 'home', component: AppComponent},
   {path: 'solicitar-anticipo', component: SolicitarAnticiposComponent},
   {path: 'mis-pendientes', component: MisPendientesComponent},
   {path: 'aprobar-anticipo', component: AprobarAnticipoComponent},
-  {path: 'consultar-anticipos', component: ConsultarAnticiposComponent}
+  {path: 'consultar-anticipos', component: ConsultarAnticiposComponent},
+  {path: 'legalizar-anticipo', component: LegalizarAnticipoComponent},
+  {path: 'desembolsar-anticipo', component: DesembolsarAnticipoComponent},
+  {path: 'editar-legalizacion', component: EditarLegalizacionComponent},
+  {path: 'aprobar-legalizacion', component: AprobarLegalizacionComponent}
 ];
 
 @NgModule({
