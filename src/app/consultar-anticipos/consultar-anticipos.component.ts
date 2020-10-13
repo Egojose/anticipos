64e5dabe4de3b03eb2e5c32d236aa40bfce237d9
Egojose <josemicrobiologo@gmail.com>
2020-10-13 18:31:08 -0500

    configura pdf y tabla de  vista inicial

--- src/app/consultar-anticipos/consultar-anticipos.component.ts
+++ src/app/consultar-anticipos/consultar-anticipos.component.ts
@@ -3,8 +3,12 @@ import { ServiciosService } from '../servcios/servicios.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { PdfMakeWrapper, Txt, Img, Columns, Table} from "pdfmake-wrapper";
+import { PdfMakeWrapper, Txt, Img, Table} from "pdfmake-wrapper";
 import pdfFonts from "pdfmake/build/vfs_fonts";
+import { NgxSpinnerService } from 'ngx-spinner';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
+
 
 PdfMakeWrapper.setFonts(pdfFonts);
 // declare var jsPDF: any;
@@ -14,7 +18,7 @@ PdfMakeWrapper.setFonts(pdfFonts);
   styleUrls: ['./consultar-anticipos.component.css']
 })
 export class ConsultarAnticiposComponent implements OnInit {
-  columnsDetalle: string[] = ['Titulo', 'Solicitante', 'FechaSolicitud', 'FechaFinalizacion', 'FechaLegalizacion', 'Anticipo', 'Legalizacion', 'Reportes', 'Facturas'];
+  columnsDetalle: string[] = ['Titulo', 'Solicitante', 'Estado', 'FechaSolicitud', 'FechaFinalizacion', 'FechaLegalizacion', 'Anticipo', 'Legalizacion', 'Reportes', 'Facturas'];
   anticipos = [];
   modalRef: BsModalRef;
   data = [];
@@ -44,9 +48,11 @@ export class ConsultarAnticiposComponent implements OnInit {
   saldoAfavorDolares: any;
   saldoAfavorEuros: any;
   datosGerente = [];
+  ver = faEye;
+  verPdf = faFilePdf;
 
 
-  constructor(public Servicios: ServiciosService, private modalService: BsModalService, public router: Router, public toastr: ToastrService) { }
+  constructor(public Servicios: ServiciosService, private modalService: BsModalService, public router: Router, public toastr: ToastrService, public spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.ConsultarEmpleados();
@@ -113,7 +119,17 @@ export class ConsultarAnticiposComponent implements OnInit {
     return suma;
   }
 
+  SumarResumenLegalizacion(arr, moneda: string) {
+    let suma = 0;
+    for(let i = 0; i < arr.length; i++) {
+      let valor = arr[i][moneda]
+      suma = suma + valor;
+    }
+    return suma;
+  }
+
   ConstruirArrayDatos(id: number) {
+    this.spinner.show();
     this.dataInicial = this.anticipos.filter((x) => x.Id === id)
     console.log(this.dataInicial)
     this.data = JSON.parse(this.dataInicial[0].DetalleLegalizacion)
@@ -137,13 +153,13 @@ export class ConsultarAnticiposComponent implements OnInit {
     this.totalAnticipoDolares = this.SumarTotales(this.arrayDetalleAnticipo, 'Dolar');
     this.totalAnticipoEuros = this.SumarTotales(this.arrayDetalleAnticipo, 'Euro');
     //totales gastos
-    this.totalGastosPesos = this.SumarTotales(arrGastos, 'Peso');
-    this.totalGastosDolares = this.SumarTotales(arrGastos, 'Dolar');
-    this.totalGastosEuros = this.SumarTotales(arrGastos, 'Euro');
+    this.totalGastosPesos = this.SumarResumenLegalizacion(arrGastos, 'Peso');
+    this.totalGastosDolares = this.SumarResumenLegalizacion(arrGastos, 'Dolar');
+    this.totalGastosEuros = this.SumarResumenLegalizacion(arrGastos, 'Euro');
     //totales saldos
-    this.totalSaldoPesos = this.SumarTotales(arrSaldos, 'Peso');
-    this.totalSaldoDolares = this.SumarTotales(arrSaldos, 'Dolar');
-    this.totalSaldoEuros = this.SumarTotales(arrSaldos, 'Euro');
+    this.totalSaldoPesos = arrSaldos[arrSaldos.length - 1].Peso;
+    this.totalSaldoDolares = arrSaldos[arrSaldos.length - 1].Dolar;
+    this.totalSaldoEuros = arrSaldos[arrSaldos.length - 1].Euro;
     //saldo a favor
     if(arraySaldoAfavor.Peso) this.saldoAfavorPesos = arraySaldoAfavor.Peso;
     if(arraySaldoAfavor.Dolar) this.saldoAfavorDolares = arraySaldoAfavor.Dolar;
@@ -151,6 +167,7 @@ export class ConsultarAnticiposComponent implements OnInit {
     setTimeout(() => {
       this.GenerafPdfLegalizacion();
       this.FormatearAprobadores(this.arrayAprobadores);
+      this.spinner.hide();
     }, 3000);
     
   }
@@ -171,16 +188,34 @@ export class ConsultarAnticiposComponent implements OnInit {
       array.push(str);
     }
     console.log(array.toString())
-    return array.toString();
+    return array.toString().replace(',', ', ');
   }
 
   async GenerafPdfLegalizacion() {
     let firmaSolicitante = await new Img(this.dataInicial[0].FirmaSolicitante).width(250).margin([0, 0, 0, 0]).build();
     let firmaDirector = await new Img(this.arrayAprobadores[0].Director.Firma).width(250).margin([0, 0, 0, 0]).build();
+    let logo = await new Img('../../assets/images/logoAraujo.png').width(200).margin([0, 0, 0, 0]).build();
+    let version = '1.0'
     console.log(2)
     const pdf = new PdfMakeWrapper();
     pdf.add(
-      new Txt(`Anticipo o Reintegro: Consecutivo ${this.dataInicial[0].Consecutivo}`).fontSize(18).bold().alignment('center').margin([10, 5]).end
+      new Table([
+        [logo, {text: 'FORMATO', alignment: 'center', padding: 'auto'}, [
+          {text: 'Código:', alignment: 'center', fontSize: 10}, {text: 'FCR-3-20-016', alignment: 'center', fontSize: 10}, {text: `Versión: ${version}`, alignment: 'center', fontSize: 10}
+        ],
+      ]
+      ]).widths([210, 210, 100,]).margin([0,2, 0, 0]).end
+    )
+    pdf.add(
+      new Table([
+        [ [{text: 'Unidad de negocio', alignment: 'center', fontSize: 10, border: [false, false, false, false]}, {text: 'Administrativa y finaciera', alignment: 'center', fontSize: 9}], {text: 'LEGALIZACIÓN DE GASTOS DE ANTICIPO O REINTEGRO', alignment: 'center', paddingTop: 3, fontSize: 9, border: [true, false, false, true]}, [
+          {text: 'Fecha de vigencia:', alignment: 'center', fontSize: 10}, {text: '2018-01-01', alignment: 'center', fontSize: 10}
+        ],
+      ]
+      ]).widths([210, 210, 100,]).margin([0,0, 0, 0]).end
+    )
+    pdf.add(
+      new Txt(`Anticipo o Reintegro: Consecutivo ${this.dataInicial[0].Consecutivo}`).fontSize(18).bold().alignment('center').margin([10, 10, 10, 5]).end
     )
     pdf.add(
       new Txt('Propósito del anticipo o reintegro').fontSize(14).bold().alignment('left').margin([20, 10]).end
@@ -283,6 +318,8 @@ export class ConsultarAnticiposComponent implements OnInit {
     );
 
     this.arrayDetalle.forEach((el) => {
+      let fechaInicio = el.fechaInicio !== 'N/A' ? this.ObtenerFormatoFecha(el.fechaInicio) : 'N/A'
+      let fechaFin = el.fechaFin !== 'N/A' ? this.ObtenerFormatoFecha(el.fechaFin) : 'N/A'
       pdf.add(
         new Table([
           [
@@ -324,12 +361,12 @@ export class ConsultarAnticiposComponent implements OnInit {
               fontSize: 9
             },{
               border: [true, false, false, true],
-              text: el.fechaInicio,
+              text: fechaInicio,
               alignment: 'center',
               fontSize: 9
             },{
               border: [true, false, true, true],
-              text: el.fechaFin,
+              text: fechaFin,
               alignment: 'center',
               fontSize: 9
             },
@@ -492,13 +529,13 @@ export class ConsultarAnticiposComponent implements OnInit {
             border: [true, true, false, false],
             text: this.dataInicial[0].Solicitante.Title,
             alignment: 'center',
-            fontSize: 8
+            fontSize: 9
           },
           {
             border: [true, true, true, false],
             text: this.arrayAprobadores[0].Director.Title,
             alignment: 'center',
-            fontSize: 8
+            fontSize: 9
           }
         ]
       ]).widths([256, 255]).margin([0,3, 0, 0]).end
@@ -517,28 +554,27 @@ export class ConsultarAnticiposComponent implements OnInit {
             border: [true, false, false, true],
             text: 'Responsable del anticipo o reintegro',
             alignment: 'center',
-            fontSize: 8
+            fontSize: 9
           },
           {
             border: [true, false, true, true],
             text: 'Autoriza',
             alignment: 'center',
-            fontSize: 8
+            fontSize: 9
           }
         ]
       ]).widths([256, 255]).margin([0,0, 0, 0]).end
     );
-    pdf.footer(
-      new Table([
-        [
-          'Esto es un footer'
-        ]
-      ])
-    )
-  
-    pdf.pageSize("A4");
-    pdf.pageMargins([40, 100, 40, 5]);
-    pdf.footer('Esto es un footer')
+    // pdf.footer(
+    //   new Table([
+    //     [
+    //       'Esto es un footer'
+    //     ]
+    //   ])
+    // )
+
+    pdf.pageSize("legal");
+    pdf.pageMargins([40, 15, 40, 5]);
     pdf.pageOrientation('portrait');
     pdf.create().open()
   }
