4b70aa83fcced1e9730320337e3a48cff1333914
Egojose <josemicrobiologo@gmail.com>
2020-10-16 16:19:40 -0500

    agrega icono warning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,8 @@ import { EditarLegalizacionComponent } from './editar-legalizacion/editar-legali
 import { AprobarLegalizacionComponent } from './aprobar-legalizacion/aprobar-legalizacion.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import {MatIconModule} from '@angular/material/icon';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AdvertenciaSolicitudesComponent } from './advertencia-solicitudes/advertencia-solicitudes.component'
 
 
 
@@ -43,7 +44,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
     LegalizarAnticipoComponent,
     DesembolsarAnticipoComponent,
     EditarLegalizacionComponent,
-    AprobarLegalizacionComponent
+    AprobarLegalizacionComponent,
+    AdvertenciaSolicitudesComponent
   ],
   imports: [
     BrowserModule,
